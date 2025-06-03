f20a54d698ad921d735dba0f1ffee1b7f4327aa4
dhanush-gif <81610433+dhanush-gif@users.noreply.github.com>
2025-06-03 18:58:01 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Which programming language is widely used for developing Android mobile applications?",
+    answer: "Java",
+    options: [
+      "Python",
+      "Java",
+      "Ruby",
+      "Swift"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which operating system is not based on the Linux kernel?",
+    answer: "macOS",
+    options: [
+      "Ubuntu",
+      "macOS",
+      "Android",
+      "CentOS",
+    ]
+  },
+  {
+    numb: 3,
+    question: "Which of the following data structures is used for implementing recursion?",
+    answer: "Stack",
+    options: [
+      "Stack",
+      "Queue",
+      "Linked List",
+      "Tree",
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which algorithm is used for sorting an array in ascending order?",
+    answer: "BubbleSort",
+    options: [
+      "QuickSort",
+      "MergeSort",
+      "BubbleSort",
+      "HeapSort",
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 6,
+    question: "What is the maximum theoretical speed of USB 3.0?",
+    answer: "10 Gbps",
+    options: [
+     "1 Gbps",
+     "5 Gbps",
+     "10 Gbps",
+     "100 Gbps",
+    ]
+  },
+  {
+    numb: 7,
+    question: "Which of the following protocols is used for secure communication over the internet?",
+    answer: " HTTPS",
+    options: [
+      "HTTP",
+      "SMTP",
+      "FTP",
+      "HTTPS",
+    ]
+  },
+  {
+    numb: 8,
+    question: "Which of the following data structures is based on the principle of Last In First Out (LIFO)?",
+    answer: "Stack",
+    options: [
+    "Stack",
+    "Queue",
+    "Linked List",
+    "Tree",
+    ]
+  },
+  {
+    numb: 9,
+    question: "Which software is used for version control and collaboration among software developers?",
+    answer: "Git",
+    options: [
+     "Microsoft Excel",
+     "Google Drive",
+     "Git",
+     "Adobe Photoshop",
+    ]
+  },
+  {
+    numb: 10,
+    question: "Which programming language is used for building web applications?",
+    answer: "JavaScript",
+    options: [
+      "Java",
+      "Python",
+      "JavaScript",
+      "C++",
+    ]
+  },
+];
